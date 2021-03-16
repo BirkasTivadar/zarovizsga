45494690d9ac0e2e0702f1782d46af8756dd9e90
BirkasTivadar <birkastivadar@gmail.com>
2021-03-16 19:16:13 +0100

    Kis csiszolgatás.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -11,20 +11,24 @@ public class WorkHours {
         String result = null;
         Path path = Path.of(file);
         try (BufferedReader br = Files.newBufferedReader(path)) {
-            String line;
-            int minHours = 24;
-            while ((line = br.readLine()) != null) {
-                int actual = Integer.parseInt(line.split(",")[1]);
-                if (actual < minHours) {
-                    minHours = actual;
-                    result = line.split(",")[0] + ": " + line.split(",")[2];
-                }
-            }
-
+            result = getString(br);
         } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file", ioe);
         }
         return result;
     }
 
+    private String getString(BufferedReader br) throws IOException {
+        String result = null;
+        String line;
+        int minHours = 24;
+        while ((line = br.readLine()) != null) {
+            int actual = Integer.parseInt(line.split(",")[1]);
+            if (actual < minHours) {
+                minHours = actual;
+                result = line.split(",")[0] + ": " + line.split(",")[2];
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
@@ -5,7 +5,6 @@ import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
