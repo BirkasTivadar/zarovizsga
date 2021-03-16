4b33adeaeaf0c117114859922d380186cb2083e6
BirkasTivadar <birkastivadar@gmail.com>
2021-03-16 18:06:29 +0100

    WorkHours kész.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -1,6 +1,30 @@
 package hu.nive.ujratervezes.zarovizsga.workhours;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class WorkHours {
 
+    public String minWork(String file) {
+        String result = null;
+        Path path = Path.of(file);
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            int minHours = 24;
+            while ((line = br.readLine()) != null) {
+                int actual = Integer.parseInt(line.split(",")[1]);
+                if (actual < minHours) {
+                    minHours = actual;
+                    result = line.split(",")[0] + ": " + line.split(",")[2];
+                }
+            }
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+        return result;
+    }
 
 }
--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkHoursTest {
-    /*
+
     @Test
     void minWork() {
         WorkHours workHours = new WorkHours();
         String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
         assertEquals("John Doe: 2021-01-04", result);
     }
-*/
+
 }
