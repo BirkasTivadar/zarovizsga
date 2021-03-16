a74e1143a92828cbf1d76b165e2f39fcd1667ba5
BirkasTivadar <birkastivadar@gmail.com>
2021-03-16 17:24:17 +0100

    DigitsCounter kész.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,7 +1,26 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
 
+    public int getCountOfDigits(String s){
+        if(isEmpty(s)){
+            return 0;
+        }
+        Set<Character> result = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) > 47 && s.charAt(i) < 57) {
+                result.add(s.charAt(i));
+            }
+        }
+        return result.size();
+    }
+
+    private boolean isEmpty(String str){
+        return (str == null || str.isBlank());
+    }
 
 
 }
